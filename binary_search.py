ed2515d72b0bc0f1daca77a5c6ff9b2c0fc64e16
b0bursoliyev <soliyevb81@gmail.com>
2023-11-05 20:08:29 +0500

    change commit

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,18 @@
-# listda yo'q elementlar berilsa error beryabdi
-myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
-# myList = [i for i in input().split()]
-print(myList)
-x = int(input())
-low = 0
-high = len(myList)
-while low<high and low>=0 and high>0:
-  m = (low+high)//2
-  if x==myList[m]:
-    out = myList.index(x)
-    break
-  elif x>myList[m]:
-    low = m+1
-  elif x<myList[m]:
-    high = m
+def bianry_search(list,item):
+  low = 0
+  high = len(list)
+  while low<high and high>0 and low>=0:
+    mid = (high+low)//2
+    guess = list[mid]
+    if guess == item:
+      return mid
+    elif guess>item:
+      high = mid - 1
+    else:
+      low = mid + 1
+  return None
 
-NameError('None')
-print("Output: ",out)
+myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
+item = 23
+#item = int(input())
+print(bianry_search(myList,item))
--- linear_search.py
+++ linear_search.py
@@ -2,11 +2,8 @@ myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
 # myList = [i for i in input().split()]
 x = int(input())
 def lin_search(myList,x):
-  for i in myList:
-    if i==x:
-      output = myList.index(i)
-      break
-    else:
-      output = None
-  return output
+  for i in range(len(myList)):
+    if myList[i]==x:
+      return i
+  return None
 print(lin_search(myList,x))
