cf1ed1dac7e7b42563ee94c75031692c2a0e2a52
b0bursoliyev <soliyevb81@gmail.com>
2023-11-13 16:44:32 +0500

    first commit

---
+++ buble_sort.py
@@ -0,0 +1,10 @@
+def b_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+arr = [2,14,6,4,3,3,15,3,18,8]
+print(b_sort(arr))
