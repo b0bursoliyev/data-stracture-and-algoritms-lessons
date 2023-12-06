e8398e60c902165ab49494c2e2579f3c9bbd0d32
b0bursoliyev <soliyevb81@gmail.com>
2023-12-06 11:20:05 +0500

    change commit

--- binary_search.py
+++ binary_search.py
@@ -1,18 +1,19 @@
-def bianry_search(list,item):
+def bianry_search(list, item):
   low = 0
   high = len(list)
-  while low<high and high>0 and low>=0:
-    mid = (high+low)//2
+  while low < high and high > 0 and low >= 0:
+    mid = (high + low) // 2
     guess = list[mid]
     if guess == item:
       return mid
-    elif guess>item:
+    elif guess > item:
       high = mid - 1
     else:
       low = mid + 1
   return None
 
-myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
+
+myList = [1, 3, 4, 6, 7, 8, 10, 12, 23, 45, 56, 78, 99]
 item = 23
-#item = int(input())
-print(bianry_search(myList,item))
+# item = int(input())
+print(bianry_search(myList, item))
--- navbat.py
+++ navbat.py
@@ -1,5 +1,5 @@
 def first():
-  a = [7, 5, 7, 5, 1, 2, 6, 1, 1, 6, 7, 8,   5, 7, 4]
+  a = [7, 5, 7, 5, 1, 2, 6, 1, 1, 6, 7, 8, 5, 7, 4]
   print(a)
   l = a[0]
   for i in a:
@@ -13,17 +13,18 @@ def second():
   l.remove(max(l))
   return l
 
+
 def min_item():
-  l = [8,5,9,6,4,2,3,1,7,5,8,1,12]
+  l = [8, 5, 9, 6, 4, 2, 3, 1, 7, 5, 8, 1, 12]
   mn = min(l)
   l[l.index(mn)] = 0
   return l
 
+
 def last():
-  l = [8,5,9,6,4,2,3,1,7,5,8,1,12]
+  l = [8, 5, 9, 6, 4, 2, 3, 1, 7, 5, 8, 1, 12]
   print(l)
   a = l[0]
   l[0] = l[-1]
   l[-1] = a
   return l
-print(last())
